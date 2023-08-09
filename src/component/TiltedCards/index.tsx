75a9b5bf0e06bff4f40a39160a3387612a69b9ce
Sampada Vyas <sampada.vyas@coditas.com>
2023-08-09 07:45:39 +0530

    added the changes

--- src/component/TiltedCards/index.tsx
+++ src/component/TiltedCards/index.tsx
@@ -1,15 +1,15 @@
-
 import TiltCardComponent from "../tiltCard/index";
-import TiltCard1 from '../../assets/images/tilt-card1.svg';
-import classes from "./tiltCard.module.scss"
-
+import TiltCard1 from "../../assets/images/tilt-card1.svg";
+import TiltCard2 from "../../assets/images/tilt-card2.svg";
+import TiltCard3 from "../../assets/images/tilt-card3.svg";
+import classes from "./tiltCard.module.scss";
 
 const TitledCards = () => {
   return (
     <div className={classes.container}>
       <TiltCardComponent
         image={TiltCard1}
-        text={''}
+        text={""}
         className={classes.TiltCard1}
       />
     </div>
